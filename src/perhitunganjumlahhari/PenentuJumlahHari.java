224ce99fb7c12d4306bc43e392a4515e8cf0e2fb
FiqriF21 <117889539+FiqriF21@users.noreply.github.com>
2022-11-30 16:42:39 +0800

    menambahkan coding main string

--- src/perhitunganjumlahhari/PenentuJumlahHari.java
+++ src/perhitunganjumlahhari/PenentuJumlahHari.java
@@ -301,7 +301,11 @@ public class PenentuJumlahHari extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PenentuJumlahHari().setVisible(true);
+            }
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
